101abf34fd929d04565d7818b0dcd4bcd5e6324a
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-10-17 23:49:29 +0900

    create Nav.js

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div>
+      <span>빵 클리커 게임</span>
+      <span>베이킹 레시피</span>
+      <span>내 주변 빵집</span>
+    </div>
+  );
+}
+
+export default Nav;
