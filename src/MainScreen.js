2831a79297b5a616bd27a90924a1754890e481a5
wjlee <000jun2626@gmail.com>
2024-10-31 18:42:58 +0900

    (MainScreen, Router): update comments

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -8,6 +8,7 @@ function MainScreen() {
             <header className='MainScreen-header'>
                 <h1>🥖🍞🥐 ppang-gadeuk 🥐🍞🥖</h1>
                 <nav className='button-container'>
+                    {/* 버튼 클릭시 url 이 /~~ 으로 변경됨. 이후 이 url 변경을 Router.js 모듈의 BrowserRouter 컴포넌트가 감지하여 url에 따른 컴포넌트를 화면에 렌더링시켜줌*/}
                     <Link to="/Map"><button>근처 빵집 찾기</button></Link>
                     <Link to="/ClickerGame"><button>클리커 게임</button></Link>
                     <Link to="/BakingClass"><button>홈베이킹 클래스</button></Link>
