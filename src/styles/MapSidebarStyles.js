467ef871f21dfd655e2786771ae88c76f42ccb01
nykim <ilsnr530@naver.com>
2024-11-20 20:05:12 +0900

    빵집찾기사이드바

---
+++ src/styles/MapSidebarStyles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  color: #e36313;
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  font-size: 40px;
+`;
+
+export const Button = styled.button`
+  background-color: #e36313;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+`;
