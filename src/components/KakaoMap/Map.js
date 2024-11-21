c17921103f0cecccf7ee23a3726241f72ac49df1
wjlee <000jun2626@gmail.com>
2024-11-21 19:46:51 +0900

    맵 컴포넌트의 레이아웃 개선: 로드뷰를 중앙에 배치하고 스타일 조정

--- src/components/KakaoMap/Map.js
+++ src/components/KakaoMap/Map.js
@@ -69,13 +69,23 @@ const Map = () => {
     }, []);
 
     return (
-        <div>
-            <div id="kakaoMap" style={{ width: '100%', height: '700px' }}></div>
+        <div style={{ position: 'relative', width: '100%', height: '700px' }}>
+            <div id="kakaoMap" style={{ width: '100%', height: '100%' }}></div>
             {roadViewPosition && (
-                <RoadView
-                    position={roadViewPosition}
-                    onClose={() => setRoadViewPosition(null)}
-                />
+                <div style={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    width: '50%',
+                    height: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    zIndex: 10
+                }}>
+                    <RoadView
+                        position={roadViewPosition}
+                        onClose={() => setRoadViewPosition(null)}
+                    />
+                </div>
             )}
         </div>
     );
