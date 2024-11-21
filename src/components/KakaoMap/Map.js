182f48c5f6d08f58aaa1d4442b1c9e77f0b9c03d
wjlee <000jun2626@gmail.com>
2024-11-21 17:17:36 +0900

    로드뷰 기능 추가(react-kakao-maps-sdk-Roadview 컴포넌트 사용 or kakao.maps.Roadview 객체를 직접 생성=>택1)

--- src/components/KakaoMap/Map.js
+++ src/components/KakaoMap/Map.js
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import RoadView from "./RoadView";
 
 const { kakao } = window;
 
 const Map = () => {
+
+    const [roadViewPosition, setRoadViewPosition] = useState(null);
+
     useEffect(() => {
         const container = document.getElementById('kakaoMap'); // 지도를 담을 영역의 DOM 객체 레퍼런스
         const lat = 37.58284829999999; // 위도
@@ -12,12 +16,13 @@ const Map = () => {
             level: 4 // map 확대 수준(high=wide)
         };
 
-        const map = new kakao.maps.Map(container, options);
+        const map = new kakao.maps.Map(container, options); //지도 객체 생성.
         //kakao.maps.Map: 지도 객체를 생성해주는 메서드. 매개변수로, 지도를 표시할 컨테이너와 중심 좌표, 확대 수준 등을 설정가능.
 
         const ps = new kakao.maps.services.Places();
         // 장소 검색 객체 ps 생성. 카카오 맵 API의 Places 서비스 객체로, 장소 검색과 관련된 여러 기능을 제공해줌. 
 
+        //new kakao.maps.LatLng(lat, lng) 해당 좌표값으로 좌표객체 생성.
         const currentPosition = new kakao.maps.LatLng(lat, lng);
         ps.keywordSearch("베이커리", placesSearchCB, {
             location: currentPosition,
@@ -26,18 +31,19 @@ const Map = () => {
 
         // let currentInfowindow = null;
 
+        //data 
         function placesSearchCB(data, status, pagination) {
             if (status === kakao.maps.services.Status.OK) {
                 for (let i = 0; i < data.length; i++) {
-                    displayMarker(data[i]);
+                    displayMarker(data[i]); //베이커리 카테고리에 해당하는 1000반경 이내에서 검색된 장소 데이터를 마커표시 함수에 전달.
                 }
             }
         }
 
-        function displayMarker(place) {
-            const marker = new kakao.maps.Marker({
-                map: map,
-                position: new kakao.maps.LatLng(place.y, place.x),
+        function displayMarker(place) { //한 빵집의 장소정보를 받음
+            const marker = new kakao.maps.Marker({ // 마커 객체 생성.
+                map: map, //현재 지도에 표시할 것.
+                position: new kakao.maps.LatLng(place.y, place.x), //파라미터로 받은 빵집의 위도와 경도로 -> 마커의 위치를 설정.
             });
 
             const infowindow = new kakao.maps.InfoWindow({
@@ -52,11 +58,26 @@ const Map = () => {
             kakao.maps.event.addListener(marker, "mouseout", function () {
                 infowindow.close();
             });
+            //마커 클릭시 로드뷰.
+            kakao.maps.event.addListener(marker, "click", function () {
+                setRoadViewPosition({
+                    lat: place.y, //위도
+                    lng: place.x, //경도
+                });
+            });
         }
     }, []);
 
     return (
-        <div id="kakaoMap" style={{ width: '100%', height: '700px' }}></div>
+        <div>
+            <div id="kakaoMap" style={{ width: '100%', height: '700px' }}></div>
+            {roadViewPosition && (
+                <RoadView
+                    position={roadViewPosition}
+                    onClose={() => setRoadViewPosition(null)}
+                />
+            )}
+        </div>
     );
 }
 
--- src/components/KakaoMap/RoadView.js
+++ src/components/KakaoMap/RoadView.js
@@ -1,17 +1,92 @@
-import { Roadview } from "react-kakao-maps-sdk"
-const RoadView = () => {
-    return (
-        <Roadview // 로드뷰를 표시할 Component
-            position={{
-                // 지도의 중심좌표
-                lat: 33.450701,
-                lng: 126.570667,
-                radius: 50,
-            }}
-            // 지도의 크기
-            style={{ width: "100%", height: "450px", }}
-        />
-    )
-}
+import React from 'react';
+import { Roadview } from 'react-kakao-maps-sdk';
+import styled from 'styled-components';
 
-export default RoadView
+const RoadViewContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 450px;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  padding: 5px 10px;
+  cursor: pointer;
+  z-index: 10;
+`;
+
+const RoadView = ({ position, onClose }) => {
+    return (
+        <RoadViewContainer>
+            <CloseButton onClick={onClose}>닫기</CloseButton>
+            <Roadview
+                position={{ lat: position.lat, lng: position.lng, radius: 50, }}
+                style={{ width: '100%', height: '100%' }}
+            />
+        </RoadViewContainer>
+    );
+};
+
+export default RoadView;
+
+//react-kakao-maps-sdk 에서 제공해주는 Roadview 컴포넌트를 사용하면 위처럼 간단하나 화질이 나쁘다.
+//아래처럼 sdk사용없이 kakao.maps 객체를 이용해서 Roadview 객체를 생성하면 코드 품질이 떨어지나 화질이 굿.
+
+// import React, { useEffect, useRef } from 'react';
+// import { Roadview } from 'react-kakao-maps-sdk';
+// import styled from 'styled-components';
+
+// const { kakao } = window;
+
+// const RoadViewContainer = styled.div`
+//   position: relative;
+//   width: 100%;
+//   height: 450px;
+// `;
+
+// const CloseButton = styled.button`
+//   position: absolute;
+//   top: 10px;
+//   right: 10px;
+//   background-color: #fff;
+//   border: 1px solid #ccc;
+//   padding: 5px 10px;
+//   cursor: pointer;
+//   z-index: 10;
+// `;
+
+// const RoadView = ({ position, onClose }) => {
+//     const roadviewRef = useRef(null);
+
+//     useEffect(() => {
+//         if (roadviewRef.current) {
+//             const roadview = new kakao.maps.Roadview(roadviewRef.current);
+//             const roadviewClient = new kakao.maps.RoadviewClient();
+
+//             roadviewClient.getNearestPanoId(
+//                 new kakao.maps.LatLng(position.lat, position.lng),
+//                 50,
+//                 (panoId) => {
+//                     if (panoId) {
+//                         roadview.setPanoId(panoId, new kakao.maps.LatLng(position.lat, position.lng));
+//                     } else {
+//                         console.error('로드뷰를 사용할 수 없는 위치입니다.');
+//                     }
+//                 }
+//             );
+//         }
+//     }, [position]);
+
+//     return (
+//         <RoadViewContainer>
+//             <CloseButton onClick={onClose}>닫기</CloseButton>
+//             <div ref={roadviewRef} style={{ width: '100%', height: '100%' }}></div>
+//         </RoadViewContainer>
+//     );
+// };
+
+// export default RoadView;
