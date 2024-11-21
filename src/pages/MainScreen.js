ad8935bfe42e72d0bb3a52a376b280c269558f04
wjlee <000jun2626@gmail.com>
2024-11-22 05:50:49 +0900

    fix: 링크를 '/Map'에서 '/BakeryMap'으로 변경

--- src/pages/MainScreen.js
+++ src/pages/MainScreen.js
@@ -16,8 +16,8 @@ function MainScreen() {
             <Header>
                 <Nav>
 
-      {/* 버튼 클릭시 url 이 /~~ 으로 변경됨. 이후 이 url 변경을 Router.js 모듈의 BrowserRouter 컴포넌트가 감지하여 url에 따른 컴포넌트를 화면에 렌더링시켜줌*/}
-                    <Link to="/Map">
+                    {/* 버튼 클릭시 url 이 /~~ 으로 변경됨. 이후 이 url 변경을 Router.js 모듈의 BrowserRouter 컴포넌트가 감지하여 url에 따른 컴포넌트를 화면에 렌더링시켜줌*/}
+                    <Link to="/BakeryMap">
                         <Button>근처 빵집 찾기</Button>
                     </Link>
                     <Link to="/ClickerGame">
