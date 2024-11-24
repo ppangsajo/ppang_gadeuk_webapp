be1903508abeed9cc0483fcb58641ccd0e0dca02
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-11-25 00:48:47 +0900

    번역본으로 수정, 카드 스타일 조정

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,27 +6,16 @@ import {
   Difficulty,
   CookingTime,
 } from '../styles/BakingRecipe/CardStyle.js';
-import '../static/bread_recipes.json';
 
-function Card({ title, imgName }) {
-  const context = require.context(
-    '../assets/images/BakingRecipeImages',
-    false,
-    /\.(jpg)$/
-  ); // require.context -> 디렉터리를 탐색, 가져올 파일의 경로를 배열로 반환
-  const images = context.keys().reduce((acc, path) => {
-    const key = path.replace('./', '').replace(/\.(jpg)$/, '');
-    acc[key] = context(path);
-    return acc;
-  }, {}); // keys()로 접근하여 파일 경로를 획득, context(path)로 해당 파일을 로드
-
-  const imgSrc = images[imgName];
+function Card({ props }) {
+  const { Title, Ingredients, Instructions, Image_Name } = props;
+  const imgPath = require(`../assets/images/BakingRecipeImages/${Image_Name}`);
 
   return (
     <>
       <CardStyle>
-        <RecipeTitle>{title}</RecipeTitle>
-        <RecipeThumbNail src={imgSrc} />
+        <RecipeTitle>{Title}</RecipeTitle>
+        <RecipeThumbNail src={imgPath} />
         <CardBottom>
           <Difficulty>난이도: 초급</Difficulty>
           <CookingTime>조리시간: 30분</CookingTime>
