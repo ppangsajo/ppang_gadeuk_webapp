4d84a272791d4d5c3c05d2da49ba716181a73d3e
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-11-27 19:15:46 +0900

    Header 교체 및 페이지 패딩 변경

--- src/styles/BakingRecipe/BakingClassStyle.js
+++ src/styles/BakingRecipe/BakingClassStyle.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const BakingClassContainer = styled.div`
   width: 100%;
   margin: 0 auto;
-  padding: 30px;
+  padding: 90px 30px;
   background-image: url('/baking-class-background.png');
   display: flex;
   justify-content: center;
