61ce117a39b482f90cb671da47660821c25602de
wjlee <000jun2626@gmail.com>
2024-11-30 07:26:04 +0900

    refactor: 사이드바 닫기동작 변경. 외부 클릭 -> 버튼 클릭

--- src/components/BakeryMap/SideBar.js
+++ src/components/BakeryMap/SideBar.js
@@ -33,20 +33,25 @@ const SideBar = ({ width = 280, places, currentAddress }) => {
         }
     };
 
-    // 사이드바 바깥쪽(외부) 클릭시 사이드바를 닫아주는 처리를하는 비동기 함수
-    const handleClose = async e => {
-        let sideArea = side.current; //side ref 객체에 저장된 DOM 엘리먼트
-        //sideChildren: 클릭된 요소 (e.target)가 사이드바 내부에 있는지 확인
-        let sideCildren = side.current.contains(e.target);
 
-        //사이드바가 열려있고 && 사이드바 외부를 클릭했을 경우, 사이드바를 닫는다 
-        if (isOpen && (!sideArea || !sideCildren)) {
-            //사이드바를 닫는 작업이 순차적으로(동기적으로) 일어나도록 await 사용.
-            await setX(width); //setX()로 사이드바 x축 위치를 변경작업이 완료될 때까지
-            await setOpen(false); // setOpen()으로 사이드바 열림/닫힘 상태 변경작업을 대기.
-            // 즉, 사이드바의 위치를 먼저 설정한 후, 사이드바의 열림/닫힘 상태를 설정하여 정확한 상태 관리를 보장하고자 async/await 사용.
+    //############################사이드바 바깥쪽 클릭시#####################################
+
+    /*
+        // 사이드바 바깥쪽(외부) 클릭시 사이드바를 닫아주는 처리를하는 비동기 함수
+        const handleClose = async e => {
+            let sideArea = side.current; //side ref 객체에 저장된 DOM 엘리먼트
+            //sideChildren: 클릭된 요소 (e.target)가 사이드바 내부에 있는지 확인
+            let sideCildren = side.current.contains(e.target);
+    
+            //사이드바가 열려있고 && 사이드바 외부를 클릭했을 경우, 사이드바를 닫는다 
+            if (isOpen && (!sideArea || !sideCildren)) {
+                //사이드바를 닫는 작업이 순차적으로(동기적으로) 일어나도록 await 사용.
+                await setX(width); //setX()로 사이드바 x축 위치를 변경작업이 완료될 때까지
+                await setOpen(false); // setOpen()으로 사이드바 열림/닫힘 상태 변경작업을 대기.
+                // 즉, 사이드바의 위치를 먼저 설정한 후, 사이드바의 열림/닫힘 상태를 설정하여 정확한 상태 관리를 보장하고자 async/await 사용.
+            }
         }
-    }
+ 
 
     // useEffect: 사이드바 컴포넌트가 마운트/언마운트될 때 handleClose 함수를 window 객체의 클릭 이벤트 리스너에 등록/해제하는 역할
     useEffect(() => {
@@ -58,9 +63,12 @@ const SideBar = ({ width = 280, places, currentAddress }) => {
             window.removeEventListener('click', handleClose);
         };
     })
+   */
+
+    //#################################################################
+
 
     // 사이드바와 버튼, 그리고 사이드바 내부 콘텐츠 jsx를 반환. 즉 사이드바 구조를 정의&반환 -> 화면에 사이드바 렌더링
-
     return (
         <Container>
             {/* side ref 객체는 바로 이 사이드바 엘리먼트를 참조중 */}
