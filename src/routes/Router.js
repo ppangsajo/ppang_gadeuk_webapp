74490e3ab76847ec929ecc15aa100c9ba4b55f3a
wjlee <000jun2626@gmail.com>
2024-11-30 00:56:55 +0900

    deploy: 패키지 배포를 위한 gh-pages 추가 및 라우터 basename 설정

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -14,7 +14,7 @@ import { GlobalStyle } from '../styles/GlobalStyle';
 //아래의 Router컴포넌트: 애플리케이션의 라우팅을 관리하며, 정의된 경로에 따라 적절한 컴포넌트를 렌더링해주는 컴포넌트
 function Router() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             {/* BrowserRouter는 리액트 애플리케이션에서 클라이언트 측 라우팅을 관리하는 최상위 컴포넌트. a 태그와는 달리 url의 변화에  따른 페이지의 새로고침 과정이 없기때문에 사용자가 쉽고 부드럽게 웹앱을 사용할 수 있도록 도움. 그 하위에 있는 모든 라우팅 관련 컴포넌트(Routes, Route 등)를 감싸줌. 브라우저의 History API를 사용하여 URL을 관리하고 URL 변경을 감지하는 컴포넌트.*/}
 
             {/* Routes 컴포넌트는 여러 Route 컴포넌트를 감싸는 형태.
@@ -33,7 +33,7 @@ function Router() {
                 <Route path="/ClickerGameTutorial/:id" element={<ClickerGameTutorial />} />
                 <Route path="/BakingClass" element={<BakingClass />} />
                 {/*<Route path="/Recipe" component={Recipe} />} */}
-                <Route path="*" element={<NotFound />} />
+
                 {/*정의되지 않은 경로로 접근하려는 경우 보여줄 컴포넌트*/}
             </Routes>
         </BrowserRouter>
