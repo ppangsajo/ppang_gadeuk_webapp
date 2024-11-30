413ebedb21888fe755189dccb623ee99c64a9272
wjlee <000jun2626@gmail.com>
2024-12-01 02:17:31 +0900

    Design: 커스텀 오버레이에 버튼 추가 및 스타일 개선

--- src/components/BakeryMap/CustomOverlayContent.js
+++ src/components/BakeryMap/CustomOverlayContent.js
@@ -1,7 +1,8 @@
+
 import React from 'react';
-import { Wrap, Info, Title, Close, Body, Desc, Ellipsis, Jibun, Img, Link, InfoAfter } from '../../styles/BakeryMap/CustomOverlayStyles';
+import { Wrap, Info, Title, Close, Body, Desc, Address, Tel, Distance, Img, ButtonWrapper, Button, InfoAfter } from '../../styles/BakeryMap/CustomOverlayStyles';
 import thumbnailImg from '../../assets/images/BakeryMap/marker.png';
-// 커스텀 오버레이 내부의 컨텐츠들 정의//
+
 const CustomOverlayContent = ({ place, closeOverlay, openRoadView, currentPosition }) => {
     const findRoadUrl = `https://map.kakao.com/link/to/${place.place_name},${place.y},${place.x}`;
     const detailPlaceUrl = `https://place.map.kakao.com/${place.id}`;
@@ -17,15 +18,15 @@ const CustomOverlayContent = ({ place, closeOverlay, openRoadView, currentPositi
                         <img src={thumbnailImg} width="73" height="70" alt="thumbnail" />
                     </Img>
                     <Desc>
-                        <Ellipsis>{place.address_name}</Ellipsis>
-                        <Jibun>전화번호: {place.phone}</Jibun>
-                        <Jibun>{place.distance}m</Jibun>
-                        <findRoadiv>
-                            <Link href={detailPlaceUrl} target="_blank">상세정보</Link>
-                            <button onClick={openRoadView} style={{ marginLeft: '10px' }}>로드뷰</button>
-                            <Link href={findRoadUrl} target="_blank">길찾기</Link>
-                        </findRoadiv>
+                        <Address>{place.address_name}</Address>
+                        <Tel>전화번호: {place.phone}</Tel>
+                        <Distance>{place.distance}m</Distance>
                     </Desc>
+                    <ButtonWrapper>
+                        <Button href={detailPlaceUrl} target="_blank">상세정보</Button>
+                        <Button onClick={openRoadView}>로드뷰</Button>
+                        <Button href={findRoadUrl} target="_blank">길찾기</Button>
+                    </ButtonWrapper>
                 </Body>
             </Info>
             <InfoAfter />
--- src/styles/BakeryMap/CustomOverlayStyles.js
+++ src/styles/BakeryMap/CustomOverlayStyles.js
@@ -3,20 +3,20 @@ import styled from 'styled-components';
 export const Wrap = styled.div`
   position: absolute;
   left: 0;
-  bottom: 40px;
-  width: 288px;
+  bottom: 80px; // 마커와 CustomOverlay간의 간격
+  width: 338px;
   height: 132px;
-  margin-left: -144px;
+  margin-left: -169px; //InfoAfter 위치
   text-align: left;
-  overflow: hidden;
-  font-size: 12px;
+  //overflow: visible;
+  font-size: 14px;
   font-family: 'Malgun Gothic', dotum, '돋움', sans-serif;
   line-height: 1.5;
 `;
 
 export const Info = styled.div`
-  width: 286px;
-  height: 120px;
+  width: 336px;
+  height: 160px; // 콘텐트 높이
   border-radius: 5px;
   border-bottom: 2px solid #ccc;
   border-right: 1px solid #ccc;
@@ -30,12 +30,16 @@ export const Info = styled.div`
 `;
 
 export const Title = styled.div`
-  padding: 5px 0 0 10px;
+  position: relative;
+  padding: 5px 30px 0 10px; /* 오른쪽 패딩 추가 */
   height: 30px;
   background: #eee;
   border-bottom: 1px solid #ddd;
-  font-size: 18px;
+  font-size: 19px;
   font-weight: bold;
+    white-space: nowrap; /* 텍스트 줄바꿈 방지 */
+  overflow: hidden; /* 넘치는 텍스트 숨김 */
+  text-overflow: ellipsis; /* 넘치는 텍스트 ...로 말줄임 */
 `;
 
 export const Close = styled.div`
@@ -55,28 +59,10 @@ export const Body = styled.div`
   overflow: hidden;
 `;
 
-export const Desc = styled.div`
-  position: relative;
-  margin: 13px 0 0 90px;
-  height: 75px;
-`;
-
-export const Ellipsis = styled.div`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`;
-
-export const Jibun = styled.div`
-  font-size: 11px;
-  color: #888;
-  margin-top: -2px;
-`;
-
 export const Img = styled.div`
   position: absolute;
   top: 6px;
-  left: 5px;
+  left: 10px;
   width: 73px;
   height: 71px;
   border: 1px solid #ddd;
@@ -84,16 +70,62 @@ export const Img = styled.div`
   overflow: hidden;
 `;
 
-export const Link = styled.a`
-  color: #5085BB;
+export const Desc = styled.div`
+  position: relative;
+  margin: 8px 0 0 100px;
+  height: auto; /* height를 auto로 변경하여 내용에 맞게 조정되도록 함 */
+`;
+
+export const Address = styled.div`
+font-size: 15px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export const Tel = styled.div`
+  font-size: 14px;
+  color: #888;
+  margin-top: -2px;
+`;
+
+export const Distance = styled.div`
+  font-size: 14px;
+  color: #888;
+  margin-top: -2px;
+`;
+
+export const ButtonWrapper = styled.div`
+  position: relative;
+  margin-top: 20px; 
+  display: flex;
+  gap: 25px; /* 버튼 사이의 간격 조정 */
+  justify-content: center; /* 버튼들 가운데 정렬 */
+`;
+
+export const Button = styled.a`
+  display: inline-block;
+  padding: 5px 10px;
+  border-radius: 3px;
+  background: #f9f9f9;
+  border: 1px solid #ddd;
+  font-size: 12px;
+  font-weight: bold;
+  color: #888;
+  cursor: pointer;
+  text-decoration: none;
+  
+  &:hover {
+    background: #e9e9e9;
+  }
 `;
 
 export const InfoAfter = styled.div`
   content: '';
   position: absolute;
-  margin-left: -12px;
+  margin-left: -10px;
   left: 50%;
-  bottom: 0;
+  //bottom: 20px;
   width: 22px;
   height: 12px;
   background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png');
