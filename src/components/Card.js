ab551a9e1a7dcd36dd3952db92695a790404f4c2
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-12-01 21:54:15 +0900

    레시피 페이지 난이도, 조리시간 반영

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ import {
 } from '../styles/BakingRecipe/CardStyle.js';
 
 function Card({ props, onClick }) {
-  const { Title, Ingredients, Instructions, Image_Name } = props;
+  const { Title, Difficult, Baking_Time, Image_Name } = props;
   const imgPath = require(`../assets/images/BakingRecipeImages/${Image_Name}`);
 
   return (
@@ -17,8 +17,10 @@ function Card({ props, onClick }) {
         <RecipeTitle>{Title}</RecipeTitle>
         <RecipeThumbNail src={imgPath} />
         <CardBottom>
-          <Difficulty>난이도: 초급</Difficulty>
-          <CookingTime>조리시간: 30분</CookingTime>
+          <Difficulty>난이도: {Difficult ? Difficult : '중급'}</Difficulty>
+          <CookingTime>
+            조리시간: {Baking_Time ? Baking_Time : '30분'}
+          </CookingTime>
         </CardBottom>
       </CardStyle>
     </>
